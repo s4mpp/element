126e7369b5e439d382069ad6c4f709c3a2936e1b
Samuel Pacheco <samuelpachecopst@gmail.com>
2023-08-15 22:47:54 -0300

    update card padding

--- views/card.blade.php
+++ views/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-component border-0 bg-white mb-3 mx-n3 mx-md-0">
     @if($title)
-        <div class="card-body">
-            <h6 class="mb-3"><strong>{{ $title }}</strong></h6>
+        <div class="card-body pb-1">
+            <h6 class="mb-0"><strong>{{ $title }}</strong></h6>
         </div>
     @endif
 
